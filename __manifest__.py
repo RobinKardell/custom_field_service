cc92cb5e61cd115d6ed5a5b0901842ea30ef31b9
Robin Kardell <carden.rk@gmail.com>
2024-12-22 06:22:16 +0100

    m

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,7 @@
     'depends': ['fieldservice'],
     'data': [
         'data/sequence.xml',
+        'data/field_service_worksheet_data.xml',
         'views/field_service_worksheet_views.xml',
         'views/field_service_worksheet_action.xml',
         'views/field_service_menus.xml',
